1e54b3a8cd298a80434910ae8058116876b02ff7
Admin <intexqwwe@gmail.com>
2024-11-01 11:53:53 +0530

    mdfd local main

--- app/src/main/java/com/example/prc/MainActivity.kt
+++ app/src/main/java/com/example/prc/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "From $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PrcTheme {
-        Greeting("Android")
+        Greeting("local main branch ")
     }
 }
